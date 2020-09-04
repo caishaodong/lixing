2da2f1d729983bbb35f2f026bf8182928908a242
caishaodong <1146631758@qq.com>
2020-09-04 09:09:06 +0800

    init

---
+++ src/main/java/com/shaoxing/lixing/LixingApplication.java
@@ -0,0 +1,13 @@
+package com.shaoxing.lixing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LixingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LixingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shaoxing/lixing/LixingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shaoxing.lixing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LixingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
