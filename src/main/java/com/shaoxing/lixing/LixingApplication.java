032267c026674911c2e3691de341833ecc0cf744
caishaodong <1146631758@qq.com>
2020-09-10 15:48:03 +0800

    启动项目生成文件

--- src/main/java/com/shaoxing/lixing/LixingApplication.java
+++ src/main/java/com/shaoxing/lixing/LixingApplication.java
@@ -1,5 +1,6 @@
 package com.shaoxing.lixing;
 
+import com.shaoxing.lixing.global.config.docs.JApiDocsConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +11,7 @@ public class LixingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LixingApplication.class, args);
+		JApiDocsConfig.generateDocs();
 	}
 
 }
